d306909e907013f9c41fdb58f417a4b152903ccf
gamon <blazetime4@gmail.com>
2021-04-30 17:27:40 +0900

    version-1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="material-kit">
+    <div :class="{ 'nav-open': NavbarStore.showNavbar }">
+      <router-view name="header" />
+      <div>
+        <!-- 여긴 index.vue -->
+        <router-view />
+      </div>
+      <router-view name="footer" />
+    </div>
+  </div>
+
+</template>
+
---
+++ src/views/Product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h2>소개컴포넌트</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
